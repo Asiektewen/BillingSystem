67e5977d6589f22a716a2675e23b660c2f6af988
uniqupine <uniquepine@gmail.com>
2014-12-03 14:13:19 -0600

    file

--- src/main/java/com/telecom/billing/Utils/ExcelUtils.java
+++ src/main/java/com/telecom/billing/Utils/ExcelUtils.java
@@ -86,8 +86,7 @@ public class ExcelUtils {
 				cd.setDestPhoneNumber(Integer.toString((int) row.getCell(3)
 						.getNumericCellValue()));
 				cd.setDuration((int) row.getCell(4).getNumericCellValue());
-				cd.setCallDate(new Date((long) row.getCell(5)
-						.getNumericCellValue()));
+				cd.setCallDate(row.getCell(5).getDateCellValue());
 				cd.setCallTime(Integer.toString((int) row.getCell(6)
 						.getNumericCellValue()));
 				objList.add(cd);
